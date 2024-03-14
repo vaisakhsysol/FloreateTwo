e79727310ce9c6b533af70fdc1fbae3eac5a85ac
vaisakhp98 <vaisakhp43@gmail.com>
2024-03-14 11:12:07 +0530

    first commit

--- js/cursor.js
+++ js/cursor.js
@@ -1,8 +1,13 @@
 gsap.set('.cursor',{xPercent : -50,yPercent :-50})
 
 let cursor = document.querySelector('.cursor')
+let clickCursor = document.querySelector('.clickCursor')
+
 let hand = document.querySelector('.hand')
+let clickHand = document.querySelector('.clickHand')
+
 let item = document.querySelector('.owl-carousel')
+let newsItemContainer = document.querySelector('.newsItemContainer')
 
 let mouseX;
 let mouseY;
@@ -11,7 +16,7 @@ window.addEventListener('mousemove', e =>{
     mouseX = e.clientX;
     mouseY = e.clientY;
 
-    gsap.to(cursor , 0.5 , {x:mouseX, y:mouseY})
+    gsap.to(cursor , 0.2 , {x:mouseX, y:mouseY})
 })
 
 item.addEventListener('mouseenter',()=>{
@@ -27,7 +32,7 @@ item.addEventListener('mouseenter',()=>{
 
 item.addEventListener('mousemove',()=>{
     // console.log("entered");
-    gsap.to(hand,1,{
+    gsap.to(hand,0.5,{
         x:mouseX,
         y:mouseY
     })
@@ -42,5 +47,38 @@ item.addEventListener('mouseleave',()=>{
         top:'40',
         left:'40',
         ease: Elastic.easeOut.config(1, 0.3)
+    })
+})
+
+
+
+newsItemContainer.addEventListener('mouseenter',()=>{
+    // console.log("entered");
+    gsap.to(clickHand,0.5,{
+        scale : 1,
+        opacity:1,
+        top:'-75px',
+        left:'-75px',
+        ease: Elastic.easeOut.config(1, 0.3)
+    })
+})
+
+newsItemContainer.addEventListener('mousemove',()=>{
+    // console.log("entered");
+    gsap.to(clickHand,1,{
+        x:mouseX,
+        y:mouseY
+    })
+})
+
+
+newsItemContainer.addEventListener('mouseleave',()=>{
+    // console.log("entered");
+    gsap.to(clickHand,0.2,{
+        scale : 0,
+        opacity:0,
+        top:'40',
+        left:'40',
+        // ease: Elastic.easeOut.config(1, 0.3)
     })
 })
